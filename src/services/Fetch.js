2dbc3753995e31feaab43ac4b349e1567c11ac01
Naveen <naveen.singisetty@xinthe.com>
2019-02-27 19:09:35 +0530

    changes

--- src/services/Fetch.js
+++ src/services/Fetch.js
@@ -1,3 +1,14 @@
 import GLOBAL from '../component/Constants.js';
 
+export const processFetchRequest = (url, method, input) => {
+    const URL = GLOBAL.BASE_URL + url;
+    
+}
 
+
+export const getUserInfo = (name) => {
+    let username = name.toLowerCase().trim();
+    const URL = `https://api.github.com/users/${username}`;
+    return fetch(URL)
+            .then((res) => res.json());
+}
